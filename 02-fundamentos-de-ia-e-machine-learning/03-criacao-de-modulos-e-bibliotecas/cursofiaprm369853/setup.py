deef2da348a4b448c175a1b6fe14289a629dcf10
Paulo Sobral <paulo.sobral@outlook.com.br>
2025-11-25 11:07:41 -0300

    feat: class 03

--- 02-fundamentos-de-ia-e-machine-learning/03-criacao-de-modulos-e-bibliotecas/cursofiaprm369853/setup.py
+++ 02-fundamentos-de-ia-e-machine-learning/03-criacao-de-modulos-e-bibliotecas/cursofiaprm369853/setup.py
@@ -10,7 +10,7 @@ setup(
     description='Descricao da sua lib cursofiaprm369853',
     author='Paulo Sobral',
     author_email='paulo@paulosobral.com.br',
-    url='https://github.com/paulosobral/fiap-pos-tech-ia-para-devs/tree/02-fundamentos-de-ia-e-machine-learning/03-criacao-de-modulos-e-bibliotecas/cursofiaprm369853',  
+    url='https://github.com/paulosobral/fiap-pos-tech-ia-para-devs/tree/02-fundamentos-de-ia-e-machine-learning/03-criacao-de-modulos-e-bibliotecas/02-fundamentos-de-ia-e-machine-learning/03-criacao-de-modulos-e-bibliotecas/cursofiaprm369853',  
     license='MIT',  
     long_description=long_description,
     long_description_content_type='text/markdown'
---
+++ 02-fundamentos-de-ia-e-machine-learning/03-criacao-de-modulos-e-bibliotecas/meu_investimento/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='meu_investimento_rm369853',
+   version='0.1',
+   packages=find_packages(),
+   install_requires=[],
+   author='Paulo Sobral',
+   author_email='paulo@paulosobral.com.br',
+   description='Uma biblioteca para cálculos de investimentos.',
+   url='https://github.com/paulosobral/fiap-pos-tech-ia-para-devs/tree/02-fundamentos-de-ia-e-machine-learning/03-criacao-de-modulos-e-bibliotecas/02-fundamentos-de-ia-e-machine-learning/03-criacao-de-modulos-e-bibliotecas/meu_investimento',
+   classifiers=[
+       'Programming Language :: Python :: 3',
+       'License :: OSI Approved :: MIT License',
+       'Operating System :: OS Independent',
+   ],
+   python_requires='>=3.6',
+)
